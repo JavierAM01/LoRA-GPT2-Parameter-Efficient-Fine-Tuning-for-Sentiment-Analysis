701d322d0b768a78ab47f948c91468e4d2b7470f
Javi Abollado <javierabollado@gmail.com>
2025-03-14 18:39:27 -0400

    creating lora linear

--- lora.py
+++ lora.py
@@ -19,16 +19,17 @@ class LoRALinear(nn.Linear):
                 ) -> None:
         
         #TODO: Initialize the inherited class, nn.linear 
+        super().__init__(in_features, out_features, bias, device=device, dtype=dtype)
 
         self.has_weights_merged = False
         if lora_rank > 0:
-            self.lora_dropout = #TODO
+            self.lora_dropout = nn.Dropout(lora_dropout)  # TODO: Initialize dropout
 
-            self.lora_scaling = #TODO
+            self.lora_scaling = lora_alpha / lora_rank  # TODO: Compute scaling factor
 
             #TODO: Fill in the "..."
-            self.lora_A = #TODO
-            self.lora_B = #TODO
+            self.lora_A = nn.Parameter(torch.randn(lora_rank, in_features, device=device, dtype=dtype))
+            self.lora_B = nn.Parameter(torch.zeros(out_features, lora_rank, device=device, dtype=dtype))
 
             self.lora_A.requires_grad = False
             self.lora_B.requires_grad = False
@@ -43,22 +44,37 @@ class LoRALinear(nn.Linear):
         if self.is_lora():
             #TODO: Initialize both lora_A and lora_B with torch.nn.init. Refer to the paper to see how each is initialize
             #Hint: lora_A is initialized using kaiming_uniform_ using negative slope (a) as math.sqrt(5)
-            raise NotImplementedError
+            nn.init.kaiming_uniform_(self.lora_A, a=math.sqrt(5))
+            nn.init.zeros_(self.lora_B)  # Initialize lora_B with zeros
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
         #TODO: return input after the forward pass
         #TODO: Remember to use dropout on the input before multiplying with lora_B and lora_A if the weights are not merged
-        raise NotImplementedError
+        if self.has_weights_merged:
+            return F.linear(input, self.weight, self.bias)
+        else:
+            result = F.linear(input, self.weight, self.bias)
+            if self.is_lora():
+                # TODO: Remember to use dropout on the input before multiplying with lora_B and lora_A if the weights are not merged
+                result += self.lora_scaling * F.linear(self.lora_dropout(input), self.lora_A @ self.lora_B)
+            return result
 
     def train(self, mode: bool = True) -> "LoRALinear":
         #TODO: Set the linear layer into train mode
         #Hint: Make sure to demerge LORA matrices if already merged
-        raise NotImplementedError
+        super().train(mode)
+        if mode and self.has_weights_merged:
+            self.weight.data -= self.lora_scaling * (self.lora_B @ self.lora_A)
+            self.has_weights_merged = False
         return self
 
     def eval(self) -> "LoRALinear":
         #TODO: Set the linear layer into eval mode
         #Hint: Make sure to merge LORA matrices if already demerged
+        super().eval()
+        if not self.has_weights_merged:
+            self.weight.data += self.lora_scaling * (self.lora_B @ self.lora_A)
+            self.has_weights_merged = True
         return self
     
     def extra_repr(self) -> str:
@@ -70,5 +86,11 @@ class LoRALinear(nn.Linear):
 def mark_only_lora_as_trainable(model: nn.Module) -> nn.Module:
     #TODO: Loop through parameters and mark some as trainable. Which ones should these be?
     #Hint: How do you mark a parameter as trainable (or not trainable)?
-    raise NotImplementedError
+    for param in model.parameters():
+        param.requires_grad = False  # Freeze all parameters
+    for module in model.modules():
+        if isinstance(module, LoRALinear):
+            module.lora_A.requires_grad = True
+            module.lora_B.requires_grad = True
     return model
+
