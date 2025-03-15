3b1ecb5fd9e0b84dbdec18b5e4ddb23675fb30ec
Javi Abollado <javierabollado@gmail.com>
2025-03-14 23:18:11 -0400

    adding runs for ex 5.4

---
+++ run/run54.sh
@@ -0,0 +1,16 @@
+
+r = (16, 128, 196)
+a = (64, 512, 784)
+
+for i in (0,1,2)
+do
+    python train.py --init_from="gpt2-medium" \
+        --out_dir="gpt-lora-r${r[i]}_a${a[i]}" \
+        --lora_rank=${r[i]} \
+        --lora_alpha=${a[i]} \
+end
+
+python train.py --init_from="gpt2-medium" \
+        --out_dir="gpt-lora-default" \
+        --lora_rank=0 \
+        --lora_dropout=0.05
