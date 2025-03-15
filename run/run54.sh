afb86a9e2a1bd0d19bfce077fd95cb512ed15962
Javi Abollado <javierabollado@gmail.com>
2025-03-14 23:26:02 -0400

    fix run.sh

--- run/run54.sh
+++ run/run54.sh
@@ -8,10 +8,14 @@ for i in 0 1 2; do
         --out_dir="gpt-lora-r${r[$i]}_a${a[$i]}" \
         --lora_rank=${r[$i]} \
         --lora_alpha=${a[$i]}
+    python generate.py --init-from="resume"
+        --out_dir="generate-gpt-lora-r${r[$i]}_a${a[$i]}" \
 done
 
 
 python train.py --init_from="gpt2-medium" \
-        --out_dir="gpt-lora-default" \
+        --out_dir="gpt-lora-full-finetune" \
         --lora_rank=0 \
         --lora_dropout=0.05
+python generate.py --init-from="resume"
+        --out_dir="generate-gpt-lora-full-finetune" \
