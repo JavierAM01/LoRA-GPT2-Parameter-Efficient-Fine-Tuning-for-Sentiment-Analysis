5c2a1da8d24312945fabfd32940f8f949f5b4e4f
Javi Abollado <javierabollado@gmail.com>
2025-03-15 01:40:34 -0400

    typo

--- run/run511.sh
+++ run/run511.sh
@@ -7,5 +7,6 @@ for p in -1 0 1 2 3 4 5 6 7 8 9; do
         --lora_alpha=512 \
         --prompt=$p
     python generate.py --init-from="resume" \
-        --out_dir="gpt-lora-prompt_$p"
+        --out_dir="gpt-lora-prompt_$p" \
+        --prompt=$p
 done
