b08c8b51ec3aef123a3244e7a30e9e3d0ac634c6
Javi Abollado <javierabollado@gmail.com>
2025-03-14 23:21:37 -0400

    fix run.sh

--- run/run54.sh
+++ run/run54.sh
@@ -1,14 +1,15 @@
+#!/bin/bash
 
-r = (16, 128, 196)
-a = (64, 512, 784)
+r=(16 128 196)
+a=(64 512 784)
 
-for i in (0,1,2)
-do
+for i in 0 1 2; do
     python train.py --init_from="gpt2-medium" \
-        --out_dir="gpt-lora-r${r[i]}_a${a[i]}" \
-        --lora_rank=${r[i]} \
-        --lora_alpha=${a[i]} \
-end
+        --out_dir="gpt-lora-r${r[$i]}_a${a[$i]}" \
+        --lora_rank=${r[$i]} \
+        --lora_alpha=${a[$i]}
+done
+
 
 python train.py --init_from="gpt2-medium" \
         --out_dir="gpt-lora-default" \
